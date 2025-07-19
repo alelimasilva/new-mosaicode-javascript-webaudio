60654409a3dbc6ea67cace80f063425f757baf41
Alexandre Lima <alexandrealima10@gmail.com>
2025-07-19 12:52:27 -0300

    Nova versão da extensão

--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-python setup.py install --record files.txt
-cat files.txt | xargs rm -rf
-rm -rf files.txt
-rm -rf build
-rm -rf dist
-rm -rf mosaicode_javascript_webaudio.egg-info
-rm -rf usr/local/lib/python2.7/dist-packages/mosaicode_javascript_webaudio-1.0-py2.7.egg
-rm -rf /usr/share/mosaicode/extensions/examples/javascript/webaudio
